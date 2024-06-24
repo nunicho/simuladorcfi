43ca34da2090ed96b4266e29c592c46ff7742a02
MauricioAlonso <program.alonso@gmail.com>
2024-06-24 15:11:18 -0300

    probando sol3

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const path = require("path");
 const scrapeRoute = require("./backend/routes/scrapeRoute");
-const cors = require("cors"); 
+//const cors = require("cors"); 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware para permitir CORS
-app.use(cors());
+//app.use(cors());
 
-/*
+const cors = require("cors");
+
 app.use(
   cors({
     origin: "https://simuladorcfi.netlify.app", // Replace with your frontend's URL
     credentials: true, // Optional, enable cookies for authenticated requests
   })
 );
-*/
 
 // Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, "./public")));
